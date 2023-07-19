3a6c6afb1071de2f9859954ff1d14f1d80d31100
wiaamhilal <weaam224112@gmail.com>
2023-07-19 03:06:59 -0700

    freinds fixed

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import help from '../views/freinds.vue'
+import freinds from '../views/freinds.vue'
 import portfolio from '../views/portfolio.vue'
 import blog from '../views/products.vue'
 import culcolater from '../views/culcolater'
@@ -22,7 +22,7 @@ const routes = [
   {
     path: "/freinds",
     name: "freinds",
-    component: help
+    component: freinds
   },
   {
     path: "/products",
